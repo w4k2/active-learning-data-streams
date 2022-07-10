c291f3bf4bb8ed55c5c7ac22ba24d3ec098b791e
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-07-11 00:43:26 +0200

    update

--- run.sh
+++ run.sh
@@ -2,50 +2,50 @@
 
 
 # variable budget
-# python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=1410
-# python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.5 --random_seed=1410
+python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
+python plots.py --stream_len=5000 --budget=0.5 --random_seed=1410 --seed_size=1000
 
-# python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.4 --random_seed=1410
-# python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.4 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.4 --random_seed=1410
+python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
+python plots.py --stream_len=5000 --budget=0.4 --random_seed=1410 --seed_size=1000
 
-# python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --random_seed=1410
-# python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.3 --random_seed=1410
+python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
+python plots.py --stream_len=5000 --budget=0.3 --random_seed=1410 --seed_size=1000
 
-# python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.2 --random_seed=1410
-# python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.2 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.2 --random_seed=1410
+python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
+python plots.py --stream_len=5000 --budget=0.2 --random_seed=1410 --seed_size=1000
 
-# python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.1 --random_seed=1410
-# python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.1 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.1 --random_seed=1410
+python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="online_bagging" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="all_labeled_ensemble" --base_model="mlp" --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
+python plots.py --stream_len=5000 --budget=0.1 --random_seed=1410 --seed_size=1000
 
 
 # variable stream len
@@ -56,7 +56,7 @@ python plots.py --stream_len=5000 --budget=0.1 --random_seed=1410
 # python main.py --method="random" --base_model="mlp"  --stream_len=10000 --budget=0.3 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=10000 --budget=0.3 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=10000 --budget=0.3 --random_seed=1410
-python plots.py --stream_len=10000 --budget=0.3 --random_seed=1410
+# python plots.py --stream_len=10000 --budget=0.3 --random_seed=1410
 
 # python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=20000 --budget=0.3 --random_seed=1410
 # python main.py --method="all_labeled" --base_model="mlp" --stream_len=20000 --budget=0.3 --random_seed=1410
@@ -65,7 +65,7 @@ python plots.py --stream_len=10000 --budget=0.3 --random_seed=1410
 # python main.py --method="random" --base_model="mlp"  --stream_len=20000 --budget=0.3 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=20000 --budget=0.3 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=20000 --budget=0.3 --random_seed=1410
-python plots.py --stream_len=20000 --budget=0.3 --random_seed=1410
+# python plots.py --stream_len=20000 --budget=0.3 --random_seed=1410
 
 # python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=50000 --budget=0.3 --random_seed=1410
 # python main.py --method="all_labeled" --base_model="mlp" --stream_len=50000 --budget=0.3 --random_seed=1410
@@ -74,7 +74,7 @@ python plots.py --stream_len=20000 --budget=0.3 --random_seed=1410
 # python main.py --method="random" --base_model="mlp"  --stream_len=50000 --budget=0.3 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=50000 --budget=0.3 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=50000 --budget=0.3 --random_seed=1410
-python plots.py --stream_len=50000 --budget=0.3 --random_seed=1410
+# python plots.py --stream_len=50000 --budget=0.3 --random_seed=1410
 
 # base model ng (naive bayes)
 # python main.py --method="ours" --base_model="ng" --beta1=0.9 --prediction_threshold=0.8 --stream_len=5000 --budget=0.5 --random_seed=45
@@ -84,7 +84,7 @@ python plots.py --stream_len=50000 --budget=0.3 --random_seed=1410
 # python main.py --method="random" --base_model="ng"  --stream_len=5000 --budget=0.5 --random_seed=45
 # python main.py --method="fixed_uncertainty" --base_model="ng" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=45
 # python main.py --method="variable_uncertainty" --base_model="ng" --prediction_threshold=0.95 --stream_len=5000 --budget=0.5 --random_seed=45
-python plots.py --stream_len=5000 --budget=0.5 --base_model="ng" --random_seed=45
+# python plots.py --stream_len=5000 --budget=0.5 --base_model="ng" --random_seed=45
 
 
 # variable seed size
@@ -95,7 +95,7 @@ python plots.py --stream_len=5000 --budget=0.5 --base_model="ng" --random_seed=4
 # python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=1410
+# python plots.py --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=1410
 
 # python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
 # python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
@@ -104,7 +104,7 @@ python plots.py --stream_len=5000 --budget=0.3 --seed_size=100 --random_seed=141
 # python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
+# python plots.py --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=1410
 
 # python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
 # python main.py --method="all_labeled" --base_model="mlp" --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
@@ -113,4 +113,4 @@ python plots.py --stream_len=5000 --budget=0.3 --seed_size=500 --random_seed=141
 # python main.py --method="random" --base_model="mlp"  --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
 # python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
 # python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
-python plots.py --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
+# python plots.py --stream_len=5000 --budget=0.3 --seed_size=1000 --random_seed=1410
