07ef285a6858b6da0a51d07cf5a8452e2f7983d8
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-07-20 14:19:07 +0200

    refactor run.sh

--- run.sh
+++ run.sh
@@ -1,69 +1,27 @@
 #! /bin/bash
 
 do_experiments () {
+    echo "executing experiments for dataset $1"
+    
     # variable budget
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.5 --random_seed=1410 --seed_size=1000
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.4 --random_seed=1410 --seed_size=1000
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.3 --random_seed=1410 --seed_size=1000
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.2 --random_seed=1410 --seed_size=1000
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.1 --random_seed=1410 --seed_size=1000
-
+    for BUDGET in 0.5 0.4 0.3 0.2 0.1;
+    do
+        echo "experiments with budget = $BUDGET"
+        python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000 --verbose=0
+        # python plots.py --dataset_name="$1" --budget=$BUDGET --random_seed=1410 --seed_size=1000
+    done
 
     # base model ng (naive bayes)
+    echo "experiments with Naive Bayes classifier"
     python main.py --method="ours" --base_model="ng" --beta1=0.9 --prediction_threshold=0.8 --dataset_name="$1" --budget=0.5 --random_seed=45 --verbose=0
     python main.py --method="all_labeled" --base_model="ng" --dataset_name="$1" --budget=0.5 --random_seed=45 --verbose=0
     python main.py --method="all_labeled_ensemble" --base_model="ng" --dataset_name="$1" --budget=0.5 --random_seed=45 --verbose=0
@@ -78,41 +36,21 @@ do_experiments () {
 
 
     # variable seed size
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.3 --seed_size=100 --random_seed=1410
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.3 --seed_size=500 --random_seed=1410
-
-    python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410 --verbose=0
-    # python plots.py --dataset_name="$1" --budget=0.3 --seed_size=1000 --random_seed=1410
+    for SEED_SIZE in 100 200 500 1000;
+    do
+        echo "experiments with seed size = $SEED_SIZE"
+        python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.6 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="all_labeled" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="all_labeled_ensemble" --base_model="mlp" --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="random" --base_model="mlp"  --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="fixed_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.95 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.9 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="vote_entropy" --base_model="mlp" --prediction_threshold=2.1 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.3 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        python main.py --method="max_disagreement" --base_model="mlp" --prediction_threshold=0.00001 --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410 --verbose=0
+        # python plots.py --dataset_name="$1" --budget=0.3 --seed_size=$SEED_SIZE --random_seed=1410
+    done
 }
 
 
