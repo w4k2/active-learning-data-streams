494c3c62123778a70748c24f2327c66bfcbaae55
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-10-19 13:46:11 +0200

    update script for big datasets

--- run_big_datasets.sh
+++ run_big_datasets.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-for RANDOM_SEED in 0 1 2 3 4 5 6 7 8 9
+for DATASET in "bank_marketing" "adult" "chess" "firewall"
 do
-    for BUDGET in 0.1 0.2 0.3 0.4 0.5;
+    for RANDOM_SEED in 0 1 2 3 4 5 6 7 8 9
     do
-        python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.99 --dataset_name="firewall" --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0
-        python main.py --method="fixed_uncertainty" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.99 --dataset_name="firewall" --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0
-        python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.85 --dataset_name="firewall" --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0
-        python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.75 --dataset_name="firewall" --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0
+        for BUDGET in 0.1 0.2 0.3 0.4 0.5;
+        do
+            python main.py --method="ours" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.99 --dataset_name=$DATASET --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0 --batch_mode --batch_size=100
+            python main.py --method="fixed_uncertainty" --base_model="mlp" --beta1=0.9 --prediction_threshold=0.99 --dataset_name=$DATASET --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0 --batch_mode --batch_size=100
+            python main.py --method="variable_uncertainty" --base_model="mlp" --prediction_threshold=0.85 --dataset_name=$DATASET --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0 --batch_mode --batch_size=100
+            python main.py --method="consensus_entropy" --base_model="mlp" --prediction_threshold=0.2 --dataset_name=$DATASET --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0 --batch_mode --batch_size=100
+            python main.py --method="classification_margin" --base_model="mlp" --prediction_threshold=0.75 --dataset_name=$DATASET --budget=$BUDGET --random_seed=$RANDOM_SEED --seed_size=1000 --verbose=0 --batch_mode --batch_size=100
+        done
     done
 done
