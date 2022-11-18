a879fbcda8065b435429cfc2e754113511a50150
jedrzejkozal <jedrzej.kozal@gmail.com>
2022-11-18 17:30:26 +0100

    update script

--- plot_base_acc.py
+++ plot_base_acc.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def main():
-    for i, begin_acc in enumerate([0.25, 0.3, 0.35, 0.4]):
+    for i, begin_acc in enumerate([0.5, 0.55, 0.6, 0.63]):
         print('begin_acc = ', begin_acc)
         acc = np.load(f'results/begin_acc_{begin_acc}_acc.npy')
         plt.plot(acc, label=f'{begin_acc}', color=f"C{i}")
